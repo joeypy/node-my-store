94290eb1568c62ecbab7a0f2871fbdb3f3943f37
Joseph Boscan <joeypc.py@gmail.com>
2022-09-25 19:50:54 -0400

    Using MYSQL database

--- config/config.js
+++ config/config.js
@@ -4,10 +4,10 @@ const config = {
   env: process.env.NODE_ENV || 'dev',
   port: process.env.PORT || 3000,
   dbHost: process.env.HOST,
-  dbPort: process.env.DB_PORT,
-  dbUser: process.env.DB_USER,
-  dbPassword: process.env.DB_PASSWORD,
-  dbName: process.env.DB_NAME,
+  dbPort: process.env.DB_MYSQL_PORT,
+  dbUser: process.env.DB_MYSQL_USER,
+  dbPassword: process.env.DB_MYSQL_PASSWORD,
+  dbName: process.env.DB_MYSQL_NAME,
 };
 
 module.exports = config;
