85e2cc32862030b1ee8e631a083e916aefaf1aab
Joseph Boscan <joeypc.py@gmail.com>
2022-09-25 22:42:37 -0400

    Fixing error connection

--- config/config.js
+++ config/config.js
@@ -7,20 +7,10 @@ const URI = `postgres://${USER}:${PASSWORD}@${process.env.DB_HOST}:${process.env
 module.exports = {
   development: {
     url: URI,
-    // username: process.env.DB_USER,
-    // password: process.env.DB_PASSWORD,
-    // database: process.env.DB_NAME,
-    // host: process.env.DB_HOST,
-    // port: process.env.DB_PORT,
     dialect: 'postgres',
   },
   production: {
     url: URI,
-    // username: process.env.DB_USER,
-    // password: process.env.DB_PASSWORD,
-    // database: process.env.DB_NAME,
-    // host: process.env.DB_HOST,
-    // port: process.env.DB_PORT,
     dialect: 'postgres',
   },
 };
--- libs/sequelize.js
+++ libs/sequelize.js
@@ -1,10 +1,11 @@
 const { Sequelize } = require('sequelize');
-const config = require('./../config/config');
 const setupModels = require('./../config');
 
-const USER = encodeURIComponent(config.dbUser);
-const PASSWORD = encodeURIComponent(config.dbPassword);
-const URI = `postgres://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
+require('dotenv').config();
+
+const USER = encodeURIComponent(process.env.DB_USER);
+const PASSWORD = encodeURIComponent(process.env.DB_PASSWORD);
+const URI = `postgres://${USER}:${PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`;
 
 const sequelize = new Sequelize(URI, {
   dialect: 'postgres',
